33b63aa9cd36a0fa5de6cf4b08d3d3457d068a99
Spielberg Hanielly <spiielberg@gmail.com>
2024-02-05 15:31:21 -0300

    create token to access stream page in dashboard

--- app/(dashboard)/u/[username]/(home)/page.tsx
+++ app/(dashboard)/u/[username]/(home)/page.tsx
@@ -1,8 +1,25 @@
-const CreatorPage = () => {
+import { StreamPlayer } from '@/components/stream-player'
+import { getUserByUsername } from '@/lib/user-service'
+import { currentUser } from '@clerk/nextjs'
+
+interface CreatorPageProps {
+  params: {
+    username: string
+  }
+}
+
+const CreatorPage = async ({ params }: CreatorPageProps) => {
+  const externalUser = await currentUser()
+  const user = await getUserByUsername(params.username)
+
+  if (!user || user.externalUserId !== externalUser?.id || !user.stream) {
+    throw new Error('Unauthorized')
+  }
+
   return (
-    <>
-      <p>CreatorPage</p>
-    </>
+    <div className="h-full">
+      <StreamPlayer user={user} isFollowing />
+    </div>
   )
 }
 
