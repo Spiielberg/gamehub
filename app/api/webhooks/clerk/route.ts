b24ceb666b7dc56b4baa40a940fb093f499cd9e1
Spielberg Hanielly <spiielberg@gmail.com>
2024-02-18 21:41:02 -0300

    reset ingress when user delete account

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,3 +1,4 @@
+import { resetIngress } from '@/actions/ingress'
 import { db } from '@/lib/db'
 import { WebhookEvent } from '@clerk/nextjs/server'
 import { headers } from 'next/headers'
@@ -79,6 +80,8 @@ export const POST = async (req: Request) => {
   }
 
   if (eventType === 'user.deleted') {
+    await resetIngress(payload.data.id)
+
     const currentUser = await db.user.findUnique({
       where: { externalUserId: payload.data.id },
     })
