ab1af33389f9edf99e41faf4667ef733d8499628
Spielberg Hanielly <spiielberg@gmail.com>
2024-02-18 21:42:16 -0300

    add search page to public routes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,13 @@
 import { authMiddleware } from '@clerk/nextjs'
 
 export default authMiddleware({
-  publicRoutes: ['/', '/:username', '/api/webhooks(.*)', '/api/uploadthing'],
+  publicRoutes: [
+    '/',
+    '/search',
+    '/:username',
+    '/api/webhooks(.*)',
+    '/api/uploadthing',
+  ],
 })
 
 export const config = {
